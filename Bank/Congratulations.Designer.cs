fb0ac325466124fcaa8114b33e31c76fc8349c36
Nick <nwright@cpi-print.co.uk>
2020-07-26 20:20:31 +0100

    Redesign for bitcoin emphasis
    Change forms to include better focus on bitcoins

--- Bank/Congratulations.Designer.cs
+++ Bank/Congratulations.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -38,10 +39,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Green;
-            this.label1.Location = new System.Drawing.Point(80, 52);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(115, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(473, 36);
+            this.label1.Size = new System.Drawing.Size(385, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Well done you found the money!";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -51,10 +51,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Green;
-            this.label2.Location = new System.Drawing.Point(122, 154);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(147, 38);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(380, 36);
+            this.label2.Size = new System.Drawing.Size(310, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "The door code is: 635271";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -64,22 +63,36 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Green;
-            this.label3.Location = new System.Drawing.Point(49, 238);
+            this.label3.Location = new System.Drawing.Point(90, 140);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(531, 72);
+            this.label3.Size = new System.Drawing.Size(437, 87);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Do you want to exit straight away or \r\nplay on and find those bitcoins?";
+            this.label3.Text = "Do you want to exit straight away or \r\nwill you complete the 2nd challenge \r\nand " +
+    "find all 25 bitcoins?";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(127, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(363, 48);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Remember you must be out of \r\nthe room BEFORE your time runs out.";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Congratulations
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 367);
+            this.ClientSize = new System.Drawing.Size(617, 375);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Congratulations";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Congratulations";
@@ -93,5 +106,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
